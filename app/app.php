f0ae4fc1c4fdb1c9d61d0d943a255b6778e2c772
Jessica Fix and Mary Warrington <marywarrington@gmail.com>
2016-02-15 10:53:33 -0800

    Add twig pages

--- app/app.php
+++ app/app.php
@@ -1,10 +1,21 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/PingPongGenerator.php";
 
     $app = new Silex\Application();
 
-    $app->get("/", function() {
-        return "Home";
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('home.html.twig');
+    });
+
+    $app->get("/results", function() use ($app) {
+        $my_PingPongGenerator = new PingPongGenerator;
+        $pingPongResult = $my_PingPongGenerator->makePingPong($_GET['input_number']);
+        return $app['twig']->render('results.html.twig', array('results'=>$pingPongResult));
     });
 
     return $app;
