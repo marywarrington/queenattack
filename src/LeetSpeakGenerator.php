65ec94102b9c93fbcbc5aa1b95e99c9e7d918736
Jessica Fix and Mary Warrington <marywarrington@gmail.com>
2016-02-15 14:26:32 -0800

    add functionality for replacing S, ignoring S at the beginning of a word

--- src/LeetSpeakGenerator.php
+++ src/LeetSpeakGenerator.php
@@ -7,60 +7,30 @@
             $input_array_of_words = explode(" ", $input);
             $output_array = array();
             foreach ($input_array_of_words as $word) {
-                $output = '';
-                $output = str_replace("e", "3", $word);
-                $output = str_replace("o", "0", $output);
-                $output = str_replace("I", "1", $output);
-                array_push($output_array, $output);
+                if ((substr($word, 0, 1) != "s") && (substr($word, 0, 1) != "S")) {
+                    $output = '';
+                    $output = str_replace("e", "3", $word);
+                    $output = str_replace("o", "0", $output);
+                    $output = str_replace("I", "1", $output);
+                    $output = str_replace("s", "z", $output);
+                    array_push($output_array, $output);
+                } else {
+                    $letter_array = str_split($word);
+                    $output_letters = array();
+                    array_push($output_letters, $letter_array[0]);
+                    array_shift($letter_array);
+                    foreach ($letter_array as $letter) {
+                        $output = '';
+                        $output = str_replace("e", "3", $letter);
+                        $output = str_replace("o", "0", $output);
+                        $output = str_replace("I", "1", $output);
+                        $output = str_replace("s", "z", $output);
+                        array_push($output_letters, $output);
+                    }
+                array_push($output_array, implode("", $output_letters));
             }
-
-            // $output = str_replace("s", "z", $output);
-            // $output = str_replace(" z", " s", $output);
-            return $output_array;
+        }
+            return implode(" ", $output_array);
         }
     }
-
-// input "somewhere over the rainbow"
-// array ["somewhere", "over", "the", "rainbow"]
-// changed ["s0m3wh3r3", ....]
-
-
-//
-// if ("s") {
-//     replace with z
-// } elseif (" s") {
-//     do nothing
-// }
-
-
-    // class PingPongGenerator
-    // {
-    //     function makePingPong($input_number)
-    //     {
-    //
-    //         $pingPongArray = array();
-    //         $i = 1;
-    //         while ($i <= $input_number) {
-    //             array_push($pingPongArray, $i);
-    //             ++$i;
-    //         }
-    //
-    //     $output_pingpong = array();
-    //     foreach ($pingPongArray as $number) {
-    //         if ($number % 15 == 0) {
-    //             array_push($output_pingpong, "pingpong");
-    //         } elseif ($number % 3 == 0) {
-    //             array_push($output_pingpong, "ping");
-    //         } elseif ($number % 5 == 0) {
-    //             array_push($output_pingpong, "pong");
-    //         } else {
-    //             array_push($output_pingpong, $number);
-    //         }
-    //     }
-    //     return $output_pingpong;
-    //
-    //     }
-    // }
-
-
  ?>
